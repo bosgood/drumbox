491d815f26bef16c772d589720a2ad25b944934d
bosgood <bosgood@gmail.com>
2016-10-11 00:06:44 -0400

    Attempting to parse WAV to portaudio

--- main.go
+++ main.go
@@ -1,29 +1,87 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"github.com/cryptix/wav"
 	"github.com/gordonklaus/portaudio"
-	"math/rand"
-	"time"
+	"io"
+	"os"
+	"os/signal"
 )
 
 func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: drumbox <file.wav>\n")
+		os.Exit(1)
+	}
+	testInfo, err := os.Stat(os.Args[1])
+	checkErr(err)
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, os.Kill)
+
+	testWav, err := os.Open(os.Args[1])
+	checkErr(err)
+
+	wavReader, err := wav.NewReader(testWav, testInfo.Size())
+	fmt.Println(wavReader.String())
+	checkErr(err)
+
+	audioData := []uint16{}
+
+	// Read in complete audio file
+	for {
+		samp, err := wavReader.ReadRawSample()
+		if err == io.EOF {
+			break
+		}
+		checkErr(err)
+
+		var sampVal uint16
+		sampBuf := bytes.NewBuffer(samp)
+		binary.Read(sampBuf, binary.LittleEndian, &sampVal)
+		audioData = append(audioData, sampVal)
+	}
+
+	numSamples := wavReader.GetSampleCount()
+
 	portaudio.Initialize()
 	defer portaudio.Terminate()
-	h, err := portaudio.DefaultHostApi()
-	chk(err)
-	stream, err := portaudio.OpenStream(portaudio.HighLatencyParameters(nil, h.DefaultOutputDevice), func(out []int32) {
-		for i := range out {
-			out[i] = int32(rand.Uint32())
+	paBuffer := make([]uint16, 8192)
+	stream, err := portaudio.OpenDefaultStream(
+		0,
+		1,
+		44100,
+		len(paBuffer),
+		&paBuffer,
+	)
+	checkErr(err)
+
+	audioDataBuffer := bytes.NewBuffer(audioData)
+
+	for remaining := int(numSamples); remaining > 0; remaining -= len(paBuffer) {
+		if len(paBuffer) > remaining {
+			paBuffer = paBuffer[:remaining]
 		}
-	})
-	chk(err)
-	defer stream.Close()
-	chk(stream.Start())
-	time.Sleep(time.Second)
-	chk(stream.Stop())
+		err := binary.Read(audioDataBuffer, binary.LittleEndian, paBuffer)
+		if err == io.EOF {
+			break
+		}
+		checkErr(err)
+		checkErr(stream.Write())
+
+		select {
+		case <-sig:
+			return
+		default:
+		}
+		stream.Write()
+	}
 }
 
-func chk(err error) {
+func checkErr(err error) {
 	if err != nil {
 		panic(err)
 	}
